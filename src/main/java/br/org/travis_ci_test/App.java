2062371ab0e541a9b2a8af21b93afbe31595fda7
João Helis Bernardo <joaohelis.bernardo@gmail.com>
2016-05-12 20:17:36 -0300

    Initial commit

---
+++ src/main/java/br/org/travis_ci_test/App.java
@@ -0,0 +1,13 @@
+package br.org.travis_ci_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
